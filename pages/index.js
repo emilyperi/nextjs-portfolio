74e5c26476ff51d6a92aefae42fa7a48df6f44a1
Emily Peri <emily@emilyperi.com>
2022-07-05 10:54:03 -0700

    "deleted unused components, created navlink component"

--- pages/index.js
+++ pages/index.js
@@ -6,9 +6,9 @@ export default function Home() {
   return (
     <Layout home>
         <section>
-              <div className="flex flex-col items-center justify-center max-w-prose mx-auto">
-                  <p>Hi there! Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-		  </div>
+          <div className="flex flex-col items-center justify-center max-w-prose mx-auto">
+            <p>Hi there! Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+		      </div>
         </section>    
     </Layout>
     );
