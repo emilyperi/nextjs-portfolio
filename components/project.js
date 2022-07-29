39a25bdf669baa1c9e12663c7e8052e98e6deeb1
Emily Peri <emily@emilyperi.com>
2022-07-29 10:17:10 -0700

    small adjustments to clamped text and read more read less feature

--- components/project.js
+++ components/project.js
@@ -5,16 +5,18 @@ export default function Project(props) {
     const [clamped, setClamped] = useState(true);
     const [showButton, setShowButon] = useState(true);
     const handleClick = () => setClamped(!clamped);
+    const firstChild = Array.isArray(props.children) ? props.children[0] : props.children;
 
     return (
         <div className="flex flex-col items-start mx-auto w-full mt-18 mb-10">
         <h1 id={props.id} className='text-4xl text-greybrown mb-4'>{props.title}</h1>
-            <div className={clamped ? "clamp" : "long-text"}>
-                {props.children}
-            </div>
-            <div className="flex w-full justify-end">
+            {clamped && firstChild
+            ? <div className="clamp">{firstChild}</div>
+            : <div> {props.children} </div>
+            }         
+            <div className="flex w-full justify-end mt-6">
             {showButton && (
-                <button onClick={handleClick} className="bg-greymagenta p-2 text-xlightgreymagenta font-medium rounded-sm">Read {clamped ? "more" : "less"}</button>
+                <button onClick={handleClick} className="bg-greymagenta p-2 text-xlightgreymagenta font-medium rounded-sm">Read {clamped ? "more..." : "less"}</button>
             )}
             </div>
         </div>
